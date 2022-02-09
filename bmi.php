14fa8bfefffc5ed4aa41cb58e969d739d7e5a5b5
Yohaniii25 <yohanii725@gmail.com>
2022-02-09 12:30:41 +0530

    modified bmi.php

--- bmi.php
+++ bmi.php
@@ -1,62 +1,55 @@
 <?php include 'header.php';?>
 
-<?php
+<?php 
+    $errh = $errw = $errg = "";
+    $height = $weight = "";
+    $bmipass = "";
+    
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        if (empty($_POST['height'])) {
+            $errh = "<span style='color:#ed4337; font-size:17px; display:block'>Height is requried</span>";
+        } else {
+            $height = validate($_POST['height']);
+        }
+    
+        if (empty($_POST['weight'])) {
+            $errw = "<span style='color:#ed4337; font-size:17px; display:block'>Weight is requried</span>";
+        } else {
+            $weight = validate($_POST['weight']);
+        }
 
-$errh = $errw = $errg = "";
-$height = $weight = "";
-$bmipass = "";
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    if (empty($_POST['height'])) {
-        $errh = "<span style ='color:#ed4337;
-        font-size:17px; display:block'>Height is required</span>";
-    } else {
-        $height = validation($_POST['height']);
+        if (empty($_POST['height'] && $_POST['weight'])) {
+            echo "";
+        } else {
+            $bmi = ($weight / ($height * $height));
+            $bmipass = $bmi;
+        }
     }
-
-    if (empty($_POST['weight'])) {
-        $errw = "<span style ='color:#ed4337;
-        font-size:17px; display:block'>Height is required</span>";
-    } else {
-        $weight = validation($_POST['weight']);
+    
+    function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
-
-    if(empty($_POST['height'] && $_POST['weight'])) {
-        echo "";
-    } else {
-        $bmi = ($weight / ($height * $height));
-        $bmipass = $bmi;
-    }
-}
-
-function validation($data) {
-    $data = trim($data);
-    $data = stripcslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
 ?>
 
-
 <h2>Check Your BMI</h2>
-
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
     <div class="section1">
         <span>Enter Your Height : </span>
-        <input type="text" name="height" autocomplete="off" placeholder="Height in meteres">
-        <?php echo $errh; ?>
+        <input type="text" name="height" autocomplete="off" placeholder="Miter"><?php echo $errh; ?>
     </div>
-
+    
     <div class="section2">
         <span>Enter Your weight : </span>
-        <input type="text" name="weight" autocomplete="off" placeholder="weight in kilograms">
-        <?php echo $errw; ?>
-    </div>
-
+        <input type="text" name="weight" autocomplete="off" placeholder="kilogram"><?php echo $errw; ?>
+    </div>    
     <div class="submit">
-        <input type="submit" name="submit" value="Check">
-        <input type="reset" value="Reset">
+        <input type="submit" name="submit" value="Check BMI">
+        <input type="reset" value="Clear">
     </div>
+    
 </form>
 
 <?php
@@ -65,25 +58,26 @@ function validation($data) {
     if (isset($_POST['submit'])){
         if ($bmipass >= 13.6 && $bmipass <= 18.5) {
             echo "<span style='color:#00203FFF; display:block; margin-top:5px ;margin-right:50px'> Low body weight. You need to gain weight by eating moderately.</span>";?>
-            <img src=".\images\h1.png" class="one"><?php
+            <img src="images/h1.png" class="one"><?php
         } elseif ($bmipass > 18.5 && $bmipass < 24.9) {
             echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The standard of good health.</span>";?>
-            <img src=".\images\h2.png" class="two"><?php
+            <img src="images/h2.png" class="two"><?php
         } elseif ($bmipass > 25 && $bmipass < 29.9) {
             echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess body weight. Exercise needs to reduce excess weight.</span>";?>
-            <img src=".\images\h3.png" class="three"><?php
+            <img src="images/h3.png" class="three"><?php
         } elseif ($bmipass > 30 && $bmipass < 34.9) {
             echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The first stage of obesity. It is necessary to choose food and exercise.</span>";?>
-            <img src=".\images\h4.png" class="four"><?php
+            <img src="images/h4.png" class="four"><?php
         } elseif ($bmipass > 35 && $bmipass < 39.9) {
             echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> The second stage of obesity. Moderate diet and exercise are required.</span>";?>
-            <img src=".\images\h5.png" class="five"><?php
+            <img src="images/h5.png" class="five"><?php
         } elseif ($bmipass >= 40) {
             echo "<span style='color:#00203FFF; display:block; margin-top:5px;margin-right:50px'> Excess fat.<b style='color:#ed4337'> Fear of death</b>. Need a doctor advice.</span>";?>
-            <img src=".\images\h6.png" class="six"><?php
+            <img src="images/h6.png" class="six"><?php
         }
     } else {
         echo "";
     }
 ?>
+
 <?php include 'footer.php';?>
